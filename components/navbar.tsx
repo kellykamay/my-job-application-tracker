1fb3b02da0240e378b57ad9a047dfeabbab37e87
Kelly <ezekiel@talkliberation.com>
2026-03-26 17:50:14 +0800

    authentication and routing

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,28 +1,87 @@
+"use client";
+
 import { Briefcase } from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { getSession, signOut } from "@/lib/auth/auth";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback } from "./ui/avatar";
+import SignOutButton from "./sign-out-btn";
+import { useSession } from "@/lib/auth/auth-client";
 
-export default function Navbar () {
+export default function Navbar() {
+  const { data: session } = useSession();
   return (
     <nav className="border-b border-gray-200 bg-white">
-        <div className="container mx-auto flex h-16 items-center px-4 justify-between">
-            <Link href="/" className="flex item-center gap-2 text-xl font-semibold text-primary">
-                <Briefcase />
-                Job Tracker
-            </Link>
-            <div className="flex items-center gap-4">
+      <div className="container mx-auto flex h-16 items-center px-4 justify-between">
+        <Link
+          href="/"
+          className="flex items-center gap-2 text-xl font-semibold text-primary"
+        >
+          <Briefcase />
+          Job Tracker
+        </Link>
+        <div className="flex items-center gap-4">
+          {session?.user ? (
+            <>
+              <Link href="/dashboard">
+                <Button
+                  variant="ghost"
+                  className="text-gray-700 hover:text-black"
+                >
+                  Dashboard
+                </Button>
+              </Link>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <div className="relative h-8 w-8 rounded-full">
+                    <Avatar className="h-8 w-8">
+                      <AvatarFallback className="bg-primary text-white">
+                        {session.user.name[0].toUpperCase()}
+                      </AvatarFallback>
+                    </Avatar>
+                  </div>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56" align="end">
+                  <DropdownMenuLabel className="font-normal">
+                    <div className="flex flex-col space-y-1">
+                      <p className="text-sm font-medium leading-none">
+                        {session.user.name}
+                      </p>
+                      <p className="text-xs leading-none text-muted-foreground">
+                        {session.user.email}
+                      </p>
+                    </div>
+                  </DropdownMenuLabel>
+                  <SignOutButton />
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </>
+          ) : (
+            <>
               <Link href="/sign-in">
-                <Button variant="ghost" className="text-gray-700 hover:text-black">
+                <Button
+                  variant="ghost"
+                  className="text-gray-700 hover:text-black"
+                >
                   Log In
                 </Button>
               </Link>
               <Link href="/sign-up">
                 <Button className="bg-primary hover:bg-primary/90">
                   Start for free
-                </Button> 
+                </Button>
               </Link>
-            </div>
+            </>
+          )}
         </div>
+      </div>
     </nav>
   );
 }
